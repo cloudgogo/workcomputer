4cf7733890bc2ee534ebd14756ba9ab76cf89372
Charles <cloudman@foxmail.com>
2018-10-09 18:16:07 +0800

    unknown change

---
+++ java/thinking in java practice/untitled/src/testForJava/class10927/HelloDate.java
@@ -0,0 +1,24 @@
+//:testForJava/class10927/HelloDate.java
+package testForJava.class10927;
+
+import java.util.Date;
+
+/** the first thinking in java example program
+ * Display a string and today`s date
+ * @author Xiangyun Gao
+ * @author lightgoes.com
+ * @version  1.0
+ */
+public class HelloDate {
+    /**Entry point to application & class
+     *@param args array of string arguments
+     *@throws exceptions No exceptions throws
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello It`s :");
+        System.out.println(new Date());
+    }
+}/*Output : (55% match)
+Hello It`s :
+Tue Oct 09 15:57:28 CST 2018
+   *///:~
